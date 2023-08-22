d8a1af86a58145cfd653fd07108e5ee3f916e3f4
Alex Redko <a.redko@mail.com>
2023-08-22 14:15:41 +0100

    add simple assert and remo pycache

--- test_shurasbot.py
+++ test_shurasbot.py
@@ -4,6 +4,7 @@ from shurasbot import ShurasBot
 class TestShurasBot(unittest.TestCase):
     def test_run_bot(self):
         bot = ShurasBot()
+        assert True is True
         bot.run_bot()
 
 if __name__ == "__main__":
